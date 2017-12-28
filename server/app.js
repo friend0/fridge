9bb2e250b7c139ebb4ef2079742afe5f8f206994
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2017-12-28 02:00:08 -0800

    fix asyn on migrations

--- server/app.js
+++ server/app.js
@@ -1,23 +1,33 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const db = require('./orm');
+const db = require("./orm");
 
 // Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
 // Answer API requests.
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
+app.get("/api", function(req, res) {
+	res.set("Content-Type", "application/json");
+	res.send('{"message":"Hello from the custom server!"}');
 });
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+app.get("*", function(request, response) {
+	response.sendFile(
+		path.resolve(__dirname, "../react-ui/build", "index.html")
+	);
 });
 
-
-db.sequelize.sync().then().catch();
-module.exports = app;
+db.sequelize
+	.sync()
+	.then(() => {
+		const PORT = process.env.PORT || 5000;
+		app.listen(PORT, function() {
+			console.log(`Listening on port ${PORT}`);
+		});
+	})
+	.catch((e) => {
+		console.log('There was an error on startup\n', e)
+	});
--- server/index.js
+++ server/index.js
@@ -1,6 +1,1 @@
-const app = require('./app');
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-});
+module.exports = require('./app')

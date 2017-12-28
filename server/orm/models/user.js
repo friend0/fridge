6212aa75bf119eb615ff8571273fa334701a24e9
Ryan A. Rodriguez <ryarodri@ucsc.edu>
2017-12-28 01:22:42 -0800

    adding sequelize

---
+++ server/orm/models/user.js
@@ -0,0 +1,39 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+  var User = sequelize.define(
+    "User",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstName: {
+        type: Sequelize.STRING
+      },
+      lastName: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    },
+    {
+      classMethods: {
+        associate: function(models) {
+          // associations can be defined here
+        }
+      }
+    }
+  );
+  return User;
+};
